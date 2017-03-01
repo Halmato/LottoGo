77e466adeca9b394fbbae364b4cc76149ac19957
Halmato <testdatatiaan@gmail.com>
2017-03-01 15:52:14 +0200

    Initial Commit

---
+++ app/src/main/java/com/twinc/halmato/lottogo/ViewPagerActivity.java
@@ -0,0 +1,268 @@
+package com.twinc.halmato.lottogo;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.util.SparseArray;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.View;
+import android.widget.TableLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.vision.CameraSource;
+import com.google.android.gms.vision.Detector;
+import com.google.android.gms.vision.text.TextBlock;
+import com.google.android.gms.vision.text.TextRecognizer;
+import com.google.gson.Gson;
+
+import java.io.IOException;
+
+public class ViewPagerActivity extends AppCompatActivity implements TabLayout.OnTabSelectedListener,
+        ViewPagerItemFragment.FragmentPagerItemCallback, ViewPagerItemFragment.SurfaceViewFragmentSetupCompletedCallback
+{
+
+    final String TAG = "ViewPagerActivity";
+    final int REQUEST_CAMERA_PERMISSION_ID = 1001;
+
+    private CameraSource cameraSource;
+
+    private TabLayout tabLayout;
+    private ViewPager pager;
+
+    private SurfaceView cameraSurfaceView;
+    private TextView resultTextView;
+
+    private static final String[] pageTitles = {"CAMERA", "Music", "Podcasts", "Other"};
+
+
+    private void findViews()
+    {
+        Log.w(TAG, "findViews: Hi" );
+        tabLayout = (TabLayout) findViewById(R.id.tbl_main_content);
+        pager = (ViewPager) findViewById(R.id.vpg_main_content);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults)
+    {
+        switch (requestCode) {
+            case REQUEST_CAMERA_PERMISSION_ID: {
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    if(ActivityCompat.checkSelfPermission(this,Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                        return;
+                    }
+
+                    try {
+                        cameraSource.start(cameraSurfaceView.getHolder());
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_pager);
+
+        getSupportActionBar().setElevation(0f);
+
+        findViews();
+
+
+        setUpPagerAndTabs();
+
+
+    }
+
+    private void setUpCamera()
+    {
+        TextRecognizer textRecognizer = new TextRecognizer.Builder(getApplicationContext()).build();
+
+        if (!textRecognizer.isOperational()) {
+            Log.w(TAG, "setUpCamera: Detector Dependencies are not yet available");
+        } else {
+            cameraSource = new CameraSource.Builder(getApplicationContext(), textRecognizer)
+                    .setFacing(CameraSource.CAMERA_FACING_BACK)
+                    .setRequestedPreviewSize(1280, 1024)
+                    .setAutoFocusEnabled(true)
+                    .build();
+
+
+            cameraSurfaceView.getHolder().addCallback(new SurfaceHolder.Callback()
+            {
+                @Override
+                public void surfaceCreated(SurfaceHolder surfaceHolder)
+                {
+                    try {
+                        if (ActivityCompat.checkSelfPermission(ViewPagerActivity.this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+
+                            ActivityCompat.requestPermissions(ViewPagerActivity.this,new String[] { Manifest.permission.CAMERA},REQUEST_CAMERA_PERMISSION_ID);
+
+                            return;
+                        }
+                        cameraSource.start(cameraSurfaceView.getHolder());
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+                @Override
+                public void surfaceChanged(SurfaceHolder surfaceHolder, int i, int i1, int i2)
+                {
+
+                }
+
+                @Override
+                public void surfaceDestroyed(SurfaceHolder surfaceHolder)
+                {
+                    cameraSource.stop();
+                }
+            });
+
+            textRecognizer.setProcessor(new Detector.Processor<TextBlock>()
+            {
+                @Override
+                public void release()
+                {
+
+                }
+                @Override
+                public void receiveDetections(Detector.Detections<TextBlock> detections)
+                {
+                    final SparseArray<TextBlock> items = detections.getDetectedItems();
+                    if(items.size() != 0) {
+                        resultTextView.post(new Runnable()
+                        {
+                            @Override
+                            public void run()
+                            {
+                               StringBuilder stringBuilder = new StringBuilder();
+                                for (int i = 0; i < items.size(); i++) {
+                                    TextBlock item = items.valueAt(i);
+                                    stringBuilder.append(item.getValue());
+                                    stringBuilder.append("\n");
+                                }
+                                resultTextView.setText(stringBuilder.toString());
+                            }
+                        });
+                    }
+                }
+            });
+        }
+    }
+
+    private void setUpPagerAndTabs(){
+
+        tabLayout.setTabTextColors(ContextCompat.getColor(this, android.R.color.white),
+                ContextCompat.getColor(this, R.color.colorAccent));
+        tabLayout.setBackgroundColor(ContextCompat.getColor(this, R.color.colorPrimary));
+
+        CustomAdapter adapter = new CustomAdapter(getSupportFragmentManager());
+        pager.setAdapter(adapter);
+        pager.setCurrentItem(1,false);
+
+        pager.addOnPageChangeListener(new ViewPager.OnPageChangeListener()
+        {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels)
+            {
+
+
+            }
+
+            @Override
+            public void onPageSelected(int position)
+            {
+
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state)
+            {
+
+            }
+        });
+
+
+        tabLayout.addOnTabSelectedListener(this);
+        tabLayout.setupWithViewPager(pager);
+        tabLayout.getTabAt(0).setIcon(android.R.drawable.ic_menu_camera);
+    }
+
+    @Override
+    public void onTabSelected(TabLayout.Tab tab) {
+
+        pager.setCurrentItem(tab.getPosition());
+    }
+
+    @Override
+    public void onTabUnselected(TabLayout.Tab tab) {
+    }
+
+    @Override
+    public void onTabReselected(TabLayout.Tab tab) {
+    }
+
+    @Override
+    public void onPagerItemClick(String message) {
+        Toast.makeText(this, message + "!", Toast.LENGTH_SHORT).show();
+    }
+
+
+
+    /*For Pagers with a smaller/static number of pages, use FragmentPagerAdapter. It keeps visited
+    caches fragments which have been opened in memory. If you require a large/dynamic number
+    of pages, use FragmentStatePagerAdapter instead.
+    */
+    public static class CustomAdapter extends FragmentPagerAdapter
+    {
+
+        public CustomAdapter (FragmentManager manager){
+            super(manager);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return ViewPagerItemFragment.getInstance(pageTitles[position]);
+        }
+
+        @Override
+        public int getCount() {
+            return pageTitles.length;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position){
+            return pageTitles[position];
+        }
+    }
+
+    @Override
+    public void surfaceViewSetupComplete(View rootView)
+    {
+        cameraSurfaceView = (SurfaceView) rootView.findViewById(R.id.surface_view);
+        resultTextView = (TextView) rootView.findViewById(R.id.result_text_view);
+
+        setUpCamera();
+    }
+
+
+
+}
---
+++ app/src/main/java/com/twinc/halmato/lottogo/ViewPagerItemFragment.java
@@ -0,0 +1,103 @@
+package com.twinc.halmato.lottogo;
+
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.SurfaceView;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class ViewPagerItemFragment extends Fragment
+{
+    private static final String PAGE_TITLE = "PAGE_TITLE";
+
+    private String pageTitle;
+
+    private FragmentPagerItemCallback callback;
+    private SurfaceViewFragmentSetupCompletedCallback cameraAttachedCallback;
+
+    // Empty Constructor
+    public ViewPagerItemFragment(){
+    }
+
+    // Static method to return Fragment
+    public static ViewPagerItemFragment getInstance(String pageTitle){
+        ViewPagerItemFragment fragment = new ViewPagerItemFragment();
+        Bundle args = new Bundle();
+        args.putString(PAGE_TITLE, pageTitle);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            this.pageTitle = getArguments().getString(PAGE_TITLE);
+        } else {
+            Log.d("TAG", "Well... F***.");
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View v;
+
+        if(pageTitle=="CAMERA") {
+
+            v = inflater.inflate(R.layout.camera_layout,container,false);
+            cameraAttachedCallback.surfaceViewSetupComplete(v);
+
+        } else {
+
+
+            v = inflater.inflate(R.layout.fragment_view_pager_item, container, false);
+            TextView content = ((TextView) v.findViewById(R.id.lbl_pager_item_content));
+            content.setText(pageTitle);
+            content.setOnClickListener(new View.OnClickListener()
+            {
+                @Override
+                public void onClick(View v)
+                {
+                    callback.onPagerItemClick(
+                            ((TextView) v).getText().toString()
+                    );
+                }
+            });
+        }
+
+        return v;
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+
+        try {
+            callback = (FragmentPagerItemCallback) context;
+            cameraAttachedCallback = (SurfaceViewFragmentSetupCompletedCallback) context;
+        } catch (ClassCastException e) {
+            throw new ClassCastException(context.toString() + "must implement Callbacks");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        callback = null;
+        cameraAttachedCallback = null;
+    }
+
+    public interface  FragmentPagerItemCallback {
+        void onPagerItemClick(String message);
+    }
+
+    public interface  SurfaceViewFragmentSetupCompletedCallback {
+        void surfaceViewSetupComplete(View rootView);
+    }
+}
